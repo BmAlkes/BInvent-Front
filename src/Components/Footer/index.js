90309d03719dd444c9b2c8e714a2f3246431ec4b
bmalkes <bmalkes@gmail.com>
2022-12-25 18:39:39 +0200

    home page

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="--flex-center --py2">
+      <p
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        All Rights Reserved. &copy; 2022
+        <br /> <p>By Bmalkes</p>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
