01522b13cdae10cbf57fc1b30b6ae8c0ea1bb0d8
bmalkes <bmalkes@gmail.com>
2023-01-05 07:31:53 +0200

    add product page

--- src/Pages/DashBoard/Dashboard.jsx
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ProductList from "../../components/product/productList/ProductList";
-import ProductSummary from "../../components/product/productSummary/ProductSummary";
+// import ProductList from "../../Components/product/productList/ProductList";
+// import ProductSummary from "../../Components/product/productSummary/ProductSummary";
 import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
 import { getProducts } from "../../redux/features/product/productSlice";
@@ -27,8 +27,8 @@ const Dashboard = () => {
 
   return (
     <div>
-      <ProductSummary products={products} />
-      <ProductList products={products} isLoading={isLoading} />
+      {/* <ProductSummary products={products} />
+      <ProductList products={products} isLoading={isLoading} /> */}
       <h2>Dashboard</h2>
     </div>
   );
